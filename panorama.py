c4ffde8a0f661a5f78e58b8bfec8b5dc9a5a8699
Krukar, John A <jakruka@sandia.gov>
2019-10-14 11:58:37 -0600

    First attempt

--- panorama.py
+++ panorama.py
@@ -28,6 +28,8 @@ import numpy as np
 import scipy as sp
 import cv2
 
+debug_count = 0
+
 
 def getImageCorners(image):
     """Return the x, y coordinates for the four corners bounding the input
@@ -55,9 +57,38 @@ def getImageCorners(image):
         this in mind and make SURE you get it right.
     """
     corners = np.zeros((4, 1, 2), dtype=np.float32)
-    # WRITE YOUR CODE HERE
-    raise NotImplementedError
+    
+    # print("Image shape= ",image.shape)
 
+    min_x = 0
+    min_y = 0
+
+    max_x = image.shape[1]
+    max_y = image.shape[0]
+
+    corners[0,0] = [min_x,min_y]
+    corners[1,0] = [min_x,max_y]
+    corners[2,0] = [max_x,min_y]
+    corners[3,0] = [max_x,max_y]
+
+    """
+    global debug_count
+
+    debug_image = np.copy(image)
+
+    debug_image[min_y,min_x] = [0,0,255]
+    debug_image[min_y,max_x] = [0,0,255]
+    debug_image[max_y,min_x] = [0,0,255]
+    debug_image[max_y,max_x] = [0,0,255]
+
+    filename = "debug_corner_" + str(debug_count) + ".png"
+    debug_count += 1
+    cv2.imwrite(filename, debug_image)
+    print(filename)
+    """
+
+    # print(corners)
+    return corners
 
 def findMatchesBetweenImages(image_1, image_2, num_matches):
     """Return the top list of matches between two input images.
@@ -145,9 +176,26 @@ def findHomography(image_1_kp, image_2_kp, matches):
     """
     image_1_points = np.zeros((len(matches), 1, 2), dtype=np.float32)
     image_2_points = np.zeros((len(matches), 1, 2), dtype=np.float32)
-    # WRITE YOUR CODE HERE.
-    raise NotImplementedError
+    
+    match_count = len(matches)
 
+    for i in range(match_count):
+
+        next_match = matches[i]
+
+        image_1_points[i,0] = image_1_kp[next_match.queryIdx].pt
+        image_2_points[i,0] = image_2_kp[next_match.trainIdx].pt
+
+    """
+    print("image_1_points = ",image_1_points)
+    print("\n\n")
+    print("image_2_points = ",image_2_points)
+    print("\n\n")
+    """
+
+    homography, mask = cv2.findHomography(image_1_points,image_2_points,method=cv2.RANSAC, ransacReprojThreshold=5.0)
+
+    return homography
 
 def getBoundingCorners(corners_1, corners_2, homography):
     """Find the coordinates of the top left corner and bottom right corner of a
@@ -202,8 +250,36 @@ def getBoundingCorners(corners_1, corners_2, homography):
             def foo():
                 return [], [], []
     """
-    # WRITE YOUR CODE HERE
-    raise NotImplementedError
+    # print("homography")
+    # print(homography)
+
+
+    # print("corners 1 - in")
+    # print(corners_1)
+
+    corners_1_out = cv2.perspectiveTransform(corners_1, homography)
+
+    # print("corners 1 - out")
+    # print(corners_1_out)
+
+    corners = np.concatenate((corners_1_out,corners_2),axis=0)
+
+    x_slice = corners[:,:,0:1]
+    y_slice = corners[:,:,1:]
+
+    min_x = np.amin(x_slice)
+    max_x = np.amax(x_slice)
+
+    min_y = np.amin(y_slice)
+    max_y = np.amax(y_slice)
+
+    min_coord = np.array(([min_x,min_y]),dtype=np.float64)
+    max_coord = np.array(([max_x, max_y]),dtype=np.float64)
+
+    # print("min_coord:", min_coord)
+    # print("max_coord:", max_coord)
+
+    return min_coord, max_coord
 
 
 def warpCanvas(image, homography, min_xy, max_xy):
@@ -272,8 +348,20 @@ def warpCanvas(image, homography, min_xy, max_xy):
     # a warning/error, or fail silently
     canvas_size = tuple(np.round(max_xy - min_xy).astype(np.int))
     # WRITE YOUR CODE HERE
-    raise NotImplementedError
 
+    translation_matrix = [[1, 0, -min_xy[0]],
+                          [0, 1, -min_xy[1]],
+                          [0, 0, 1]]
+
+    transform_matrix = np.dot(translation_matrix, homography)
+
+    print(canvas_size)
+
+    # image_out = np.zeros(canvas_size).astype(np.uint8)
+
+    image_out = cv2.warpPerspective(image, transform_matrix, dsize=canvas_size)
+
+    return image_out
 
 def blendImagePair(image_1, image_2, num_matches):
     """This function takes two images as input and fits them onto a single
@@ -322,8 +410,7 @@ def blendImagePair(image_1, image_2, num_matches):
         input parameters, return values, etc. -- to develop your blending
         process.
     """
-    kp1, kp2, matches = findMatchesBetweenImages(
-        image_1, image_2, num_matches)
+    kp1, kp2, matches = findMatchesBetweenImages(image_1, image_2, num_matches)
     homography = findHomography(kp1, kp2, matches)
     corners_1 = getImageCorners(image_1)
     corners_2 = getImageCorners(image_2)
@@ -331,8 +418,8 @@ def blendImagePair(image_1, image_2, num_matches):
     output_image = warpCanvas(image_1, homography, min_xy, max_xy)
     # WRITE YOUR CODE HERE - REPLACE THIS WITH YOUR BLENDING CODE
     min_xy = min_xy.astype(np.int)
-    output_image[-min_xy[1]:-min_xy[1] + image_2.shape[0],
-                 -min_xy[0]:-min_xy[0] + image_2.shape[1]] = image_2
+    output_image[-min_xy[1]:-min_xy[1] + image_2.shape[0],-min_xy[0]:-min_xy[0] + image_2.shape[1]] = image_2
 
     return output_image
+    # return None
     # END OF FUNCTION
